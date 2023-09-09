3b5edbef4c5baca1c666c06eece5fd08400ff1c6
Leandro Amaral <lramaral02m@gmail.com>
2023-09-09 00:18:04 -0300

    Retornando Listas de objetos, e realizando requisições Http

---
+++ Aula02/src/main/java/br/com/erudio/model/Person.java
@@ -0,0 +1,54 @@
+package br.com.erudio.model;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String gender;
+
+    public Person() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setId(long l) {
+       }
+}
